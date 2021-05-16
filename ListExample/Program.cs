c8a12235c210424e4c91303c32a85c714d734a9e
Alican Demirtas <demirtasdev@hotmail.com>
2021-05-16 20:28:26 +0100

    Searching List Content

--- ListExample/Program.cs
+++ ListExample/Program.cs
@@ -7,7 +7,7 @@ namespace ListExample
     {
         private static void Main(string[] args)
         {
-            // BASIC LIST OPERATIONS
+            // BASIC OPERATIONS
             // Define some strings to add to the list.
             string[] comedians =
             {
@@ -39,6 +39,29 @@ namespace ListExample
             // Sort the list content.
             strList.Sort();
             PrintList(strList);
+
+
+            // SEARCHING LIST CONTENT
+            // Determine if a list contains a given item.
+            Console.WriteLine($"Is Found by `Contains`: {strList.Contains("Ron Gallagher")}");
+
+            // Determine if a list contains any items that conform to a given condition.
+            var found = strList.Exists(x => x.Length > 15);
+            Console.WriteLine($"Is Found by `Exists`: {found}");
+
+            // Return the first item (if any) in a list that conforms to a given condition.
+            var item = "";
+            item = strList.Find(x => x.StartsWith('L'));
+            Console.WriteLine($"Item Returned by `Found`: {item}");
+
+            // Return all items (if any) in a list that conform to a given condition.
+            var itemList = new List<string>();
+            itemList = strList.FindAll(x => x.StartsWith('L'));
+            PrintList(itemList);
+
+            // Determine whether all items in a list conform to a given condition.
+            bool result = strList.TrueForAll(x => x.Length > 10);
+            Console.WriteLine($"All True: {result}");
         }
 
         private static void PrintList(IEnumerable<string> list)
